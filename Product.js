bc688a46dd4a2b48ea22f681e1a91560461e3391
starrynightmobiles <starrynight2587@gmail.com>
2026-03-22 23:51:03 +0800

    Delete Product.js

--- Product.js
+++
@@ -1,9 +0,0 @@
-
-const mongoose=require("mongoose")
-const schema=new mongoose.Schema({
- name:String,
- price:Number,
- description:String,
- image:String
-})
-module.exports=mongoose.model("Product",schema)
