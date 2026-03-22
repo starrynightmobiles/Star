45eaf64f14b05dd7cd1fdda6e568aa0da6e85a48
starrynightmobiles <starrynight2587@gmail.com>
2026-03-23 00:21:30 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-
-import Hero from "../components/layout/Hero"
+import HeroSmoke from "../components/HeroSmoke"
 
 export default function Home(){
  return(
-  <div>
-   <Hero/>
-   <h1 style={{textAlign:"center"}}>Starry Mobile Space</h1>
+  <div style={{overflow:"hidden"}}>
+
+   <HeroSmoke />
+
+   <div style={{
+    position:"absolute",
+    top:"40%",
+    width:"100%",
+    textAlign:"center",
+    color:"white"
+   }}>
+    <h1 style={{fontSize:"40px"}}>Starry Mobile Space</h1>
+    <a href="/shop" style={{
+     padding:"10px 20px",
+     background:"#fff",
+     color:"#000",
+     borderRadius:"5px",
+     textDecoration:"none"
+    }}>
+     Enter Store
+    </a>
+   </div>
+
   </div>
  )
 }
