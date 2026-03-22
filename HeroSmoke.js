07f06f12b1dfaf92b3976f669e69e9558b192862
starrynightmobiles <starrynight2587@gmail.com>
2026-03-23 00:19:11 +0800

    Update HeroSmoke.js

--- HeroSmoke.js
+++ HeroSmoke.js
@@ -1,1 +1,34 @@
+export default function HeroSmoke(){
 
+ return(
+  <div style={{
+   position:"relative",
+   height:"100vh",
+   width:"100%"
+  }}>
+
+  <div style={{
+ position:"absolute",
+ width:"100%",
+ height:"100%",
+ background:"rgba(0,0,0,0.4)"
+}} />
+  <video
+    autoPlay
+    muted
+    loop
+    playsInline
+    style={{
+     position:"absolute",
+     width:"100%",
+     height:"100%",
+     objectFit:"cover",
+     zIndex:-1
+    }}
+   >
+    <source src="/videos/Smoke.mp4" type="video/mp4" />
+   </video>
+
+  </div>
+ )
+}
